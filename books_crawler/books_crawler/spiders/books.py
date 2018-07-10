c18910c88b1579dd110940416bd6c7d60998e79a
rogerroger <rogerpuichungyau@gmail.com>
2018-07-10 18:58:38 +0800

    no change commit

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -12,33 +12,42 @@ class BooksSpider(Spider):
     name = 'books'
     start_url = ['books.toscrape.com']
 
-    def start_requests(self):
-        self.driver = webdriver.Chrome('/home/roger/scrapy-2/chromedriver')
-        self.driver.get('http://books.toscrape.com')
+    # def start_requests(self):
+    #     self.driver = webdriver.Chrome('/home/roger/scrapy-2/chromedriver')
+    #     self.driver.get('http://books.toscrape.com')
 
-        sel = Selector(text=self.driver.page_source)
-        books = sel.xpath('//h3/a/@href').extract()
-        for book in books:
-            url = 'http://books.toscrape.com/' + book
-            yield Request(url, callback=self.parse_book)
+    #     sel = Selector(text=self.driver.page_source)
+    #     books = sel.xpath('//h3/a/@href').extract()
+    #     for book in books:
+    #         url = 'http://books.toscrape.com/' + book
+    #         yield Request(url, callback=self.parse_book)
 
-        while True:
-            try:
-                next_page = self.driver.find_element_by_xpath('//a[text()="next"]')
-                sleep(3)
-                self.logger.info('Sleeping for 3 seconds.')
-                next_page.click()
+        
+    #     count = 0
 
-                sel = Selector(text=self.driver.page_source)
-                books = sel.xpath('//h3/a/@href').extract()
-                for book in books:
-                    url = 'http://books.toscrape.com/catalogue/' + book
-                    yield Request(url, callback=self.parse_book)
+    #     #set stop page
+    #     till_page = 3 
 
-            except NoSuchElementException:
-                self.logger.info('No more pages to load.')
-                self.driver.quit()
-                break
+
+    #     while True and count < till_page-1:
+    #         try:
+    #             next_page = self.driver.find_element_by_xpath('//a[text()="next"]')
+    #             sleep(3)
+    #             self.logger.info('Sleeping for 3 seconds.')
+    #             next_page.click()
+
+    #             sel = Selector(text=self.driver.page_source)
+    #             books = sel.xpath('//h3/a/@href').extract()
+    #             for book in books:
+    #                 url = 'http://books.toscrape.com/catalogue/' + book
+    #                 yield Request(url, callback=self.parse_book)
+
+    #         except NoSuchElementException:
+    #             self.logger.info('No more pages to load.')
+    #             self.driver.quit()
+    #             break
+            
+    #         count+=1
 
     def parse_book(self, response):
         pass
